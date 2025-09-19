059e0e8b09100406dd2241833ced65c9705bfb92
ernos <max.cophet@gmail.com>
2025-09-19 04:24:42 +0200

    New fanxy install script

--- install.sh
+++ install.sh
@@ -1,4 +1,32 @@
 #!/bin/bash
-mkdir ~/bin
-ln -s $PWD/easycrypt.py ~/bin/easycrypt
-ln -s $PWD/easycryptconfig.json ~/bin/easycryptconfig.json
+BIN="$HOME/bin"
+EXTDIR="$HOME/.local/share/nautilus-python/extensions"
+EASYCRYPTFOLDER="$PWD"
+BINFOLDER="~/bin"
+EASYCRYPT="easycrypt.py"
+NAUTEXTENS="easycrypt-nautilus.py"
+EASYCRYPTCONFIG="easycryptconfig.json"
+
+
+# Create ~/bin if it doesn't exist
+if [[ ! -d "$BIN" ]]; then
+    echo "Creating directory $BIN"
+    mkdir -p "$BIN"
+fi
+
+echo "Installing EasyCrypt..."
+echo "Creating symlinks for local bin folder"
+
+# Symlink main script and config to ~/bin
+ln -sf "${EASYCRYPTFOLDER}/${EASYCRYPT}" "${BINFOLDER}/${EASYCRYPT}"
+ln -sf "${EASYCRYPTFOLDER}/${EASYCRYPTCONFIG}" "${BINFOLDER}/${EASYCRYPTCONFIG}"
+
+# Create Nautilus extension directory if it doesn't exist
+if [[ ! -d "$EXTDIR" ]]; then
+    echo "Creating directory $EXTDIR"
+    mkdir -p "$EXTDIR"
+
+fi
+echo "Creating symlink for nautilus context menu extension"
+# Symlink Nautilus extension script
+ln -sf "${EASYCRYPTFOLDER}/nautilus-python/extensions/${NAUTEXTENS}" "${EXTDIR}/${NAUTEXTENS}"
